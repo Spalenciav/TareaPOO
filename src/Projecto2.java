7d0ed05b9e46c3e4829c2defb6956c5ee3c94c75
Sgame <Sgame@Sp>
2024-11-12 17:44:13 -0500

    Practica

---
+++ src/Projecto2.java
@@ -0,0 +1,7 @@
+// Autor: Santiago Palencia 
+// Tema: Clase1
+public class Projecto2 {					
+    static public void main(String[] args) {			
+        System.out.println("Hola Mundo");                       
+    }
+}
